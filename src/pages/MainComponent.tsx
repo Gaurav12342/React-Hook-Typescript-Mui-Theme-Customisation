fd83e0285d02bdb5538eca24e6197700800630ab
Gaurav Pendharkar <gauravpendharkar2@gmail.com>
2022-05-01 19:06:11 +0530

    Added useMemo demo

--- src/pages/MainComponent.tsx
+++ src/pages/MainComponent.tsx
@@ -1,4 +1,4 @@
-import React, { useState, FC } from 'react'
+import React, { useState, FC, useMemo } from 'react'
 import Home from '../pages/Home';
 import Typography from '@mui/material/Typography';
 import Button from '../component/Button';
@@ -7,15 +7,26 @@ import Button from '../component/Button';
 const MainComponent: FC = () => {
   const [count, setCount] = useState(0);
   const [item, setItem] = useState(10);
+
+  const mainComponentMemo = useMemo(() => {
+    console.warn("Count multiplication");
+    return count * 5
+  }, [count]);
+
   return (
     <div>
       <Typography variant="h5" gutterBottom component="div">
         {`( Root component )`} {`Update count value: ${count}`}
       </Typography>
-      <Home item={item} />
+      <Home item={item} /> <br />
+
+      <Typography variant="h5" gutterBottom component="div">
+        {`Count multiplication: ${mainComponentMemo}`}
+      </Typography>
 
       <Button variant="contained" sx={{ backgroundColor: 'primary.dark' }} onClick={() => { setCount(count + 1) }}>{'Update Count'}</Button>&nbsp;
-      <Button variant="contained" sx={{ backgroundColor: 'status.danger' }} onClick={() => { setItem(item * 2) }}>{'Update Item'}</Button>
+      <Button variant="contained" sx={{ backgroundColor: 'status.danger' }} onClick={() => { setItem(item + 1) }}>{'Update Item'}</Button>
+
     </div>
   )
 }
